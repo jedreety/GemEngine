aa0d4219c6d6f1969605b8e472d518ead82871c3
jedreety <130364840+jedreety@users.noreply.github.com>
2025-02-01 17:29:33 +0100

    Created a protected inclulde path for the engine only

--- GemEngine/Build-Engine.lua
+++ GemEngine/Build-Engine.lua
@@ -1,13 +1,15 @@
 project "GemEngine"
-   location( _SCRIPT_DIR )
-   kind "StaticLib"
-   language "C++"
-   cppdialect "C++20"
-   targetdir "Build/%{cfg.buildcfg}"
-   staticruntime "off"
+location( _SCRIPT_DIR )
+kind "StaticLib"
+language "C++"
+cppdialect "C++20"
+targetdir "Build/%{cfg.buildcfg}"
+staticruntime "off"
 
-   files { 
-    "GemCore/include/Gem/Core/**.h", 
+files { 
+    "GemCore/include/Gem/Core/**.h",
+    "GemCore/protected-include/Gem/Core/**.h",
+
     "GemCore/src/**.cpp",
     
     "GemInput/include/Gem/Input/**.h", 
@@ -17,47 +19,48 @@ project "GemEngine"
     "GemWindow/src/**.cpp",
 }
 
-   includedirs
-   {
-      "GemCore/include",
-      "GemInput/include",
-      "GemWindow/include",
-      "C:/glfw-3.4/include",
-      "C:/VulkanSDK/1.3.296.0/Include",
-      "C:/glm-1.0.1/glm"
-   }
+includedirs
+{
+    "GemCore/include",
+    "GemCore/protected-include",
+    "GemInput/include",
+    "GemWindow/include",
+    "C:/glfw-3.4/include",
+    "C:/VulkanSDK/1.3.296.0/Include",
+    "C:/glm-1.0.1/glm"
+}
 
-   libdirs {
+libdirs {
     "C:/glfw-3.4/build/src/Debug",
     "C:/VulkanSDK/1.3.296.0/Lib"
     }
 
-   links {
+links {
 
     "glfw3",
     "vulkan-1"
     }
 
-   targetdir ("../Build/" .. OutputDir .. "/%{prj.name}")
-   objdir ("../Build/Intermediates/" .. OutputDir .. "/%{prj.name}")
+targetdir ("../Build/" .. OutputDir .. "/%{prj.name}")
+objdir ("../Build/Intermediates/" .. OutputDir .. "/%{prj.name}")
 
-   filter "system:windows"
-       systemversion "latest"
-       defines { }
+filter "system:windows"
+    systemversion "latest"
+    defines { }
 
-   filter "configurations:Debug"
-       defines { "DEBUG" }
-       runtime "Debug"
-       symbols "On"
+filter "configurations:Debug"
+    defines { "DEBUG" }
+    runtime "Debug"
+    symbols "On"
 
-   filter "configurations:Release"
-       defines { "RELEASE" }
-       runtime "Release"
-       optimize "On"
-       symbols "On"
+filter "configurations:Release"
+    defines { "RELEASE" }
+    runtime "Release"
+    optimize "On"
+    symbols "On"
 
-   filter "configurations:Dist"
-       defines { "DIST" }
-       runtime "Release"
-       optimize "On"
-       symbols "Off"
+filter "configurations:Dist"
+    defines { "DIST" }
+    runtime "Release"
+    optimize "On"
+    symbols "Off"
